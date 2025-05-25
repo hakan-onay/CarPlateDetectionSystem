42310c4991477d6f6ef58c06660737f0e5d17186
eren şahan <134325280+erensahan1@users.noreply.github.com>
2025-05-25 22:19:07 +0300

    deneme

--- Main.py
+++ Main.py
@@ -1,6 +1,6 @@
 from CarPlateDetector import CarPlateDetector
 
-# Kullanım örneği
+#
 if __name__ == "__main__":
     detector = CarPlateDetector()
     image_path = "C:/Users/Hakan/Desktop/stanadart-tip-800x800.jpg"  # Buraya görüntü dosyanın tam yolunu yaz
